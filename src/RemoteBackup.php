2f8180e03b4bd0b8496431f57f0ed4d1b7098a69
Timmy O'Mahony <hey@timmyomahony.com>
2020-11-07 15:14:30 +0000

    Updated path

--- src/RemoteBackup.php
+++ src/RemoteBackup.php
@@ -25,7 +25,7 @@ use weareferal\remotebackup\models\Settings;
 use weareferal\remotebackup\services\PruneService;
 
 use weareferal\remotecore\RemoteCoreHelper;
-use weareferal\remotecore\assets\remotecoresettings\RemoteCoreSettingsAsset;
+use weareferal\remotecore\assets\RemoteCoreSettings\RemoteCoreSettingsAsset;
 
 
 class RemoteBackup extends Plugin
